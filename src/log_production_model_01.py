b76f709d5441a84ae9595ed02cab522a83505acc
Xerxez <info@xerxez.in>
2023-11-01 09:59:00 +0530

    Data Base Creation

--- src/log_production_model_01.py
+++ src/log_production_model_01.py
@@ -1,4 +1,3 @@
-from distutils.command.config import config
 from http import client
 from get_data import read_params
 import argparse
@@ -7,8 +6,6 @@ from mlflow.tracking import MlflowClient
 from pprint import pprint
 import joblib
 import os
-import mlflow.sklearn
-
 
 def log_production_model(config_path):
     config=read_params(config_path)
@@ -21,26 +18,23 @@ def log_production_model(config_path):
     lowest_run_id=runs[runs["metrics.mae"]==lowest]["run_id"][0]
     client=MlflowClient()
     for mv in client.search_model_versions(f"name='{model_name}'"):
-        mv=dict(mv)
+        mv = dict(mv)
         if mv["run_id"]==lowest_run_id:
             current_version=mv["version"]
             logged_model=mv["source"]
             pprint(mv, indent=4)
 
-            client.transition_model_version_stage(name=model_name, version=current_version, stage="Production")
-
+            client.transition_model_version_stage(name=model_name,version=current_version,stage="Production")
         else:
             current_version=mv["version"]
-            client.transition_model_version_stage(name=model_name, version=current_version, stage="Staging")
-    
-    load_model=mlflow.pyfunc.log_model(logged_model)
-    model_path=config["model_dir"]
-    os.system(f"attrib +h {model_path}")
-    file_ = open(model_path, "wb")
+            client.transition_model_version_stage(name=model_name,version=current_version, stage="Staging")
+
+    load_model=mlflow.pyfunc.load_model(logged_model)
+    model_path=config["webapp_model_dir"]
+    joblib.dump(load_model,model_path)
 
 
-    joblib.dump(load_model, model_path)
-    
+
 
 
 if __name__=="__main__":
--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'firstApp',
+    'firstApp.apps.FirstappConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'internship',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Tanzeem@12345',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
