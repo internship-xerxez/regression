1fd90b9a9d827a099a538f38aeda4487be4ba793
Xerxez <info@xerxez.in>
2023-11-03 18:56:57 +0530

    Docker compose file 2

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'internship',
@@ -83,6 +83,17 @@ DATABASES = {
         'PASSWORD' : 'Tanzeem@12345',
         'HOST' : 'localhost',
         'PORT' : '5432',
+    }
+}'''
+
+DATABASE = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER' : os.environ.get('DB_USER'),
+        'PASSWORD' : os.environ.get('DB_PASS'),
+        'HOST' : os.environ.get('DB_HOST'),
+        #'PORT' : '5432',
     }
 }
 
