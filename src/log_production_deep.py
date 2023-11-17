2e961ecd263cfff139aef3295b45caa000a0f4d0
Xerxez <info@xerxez.in>
2023-11-17 19:17:22 +0530

    Webapp for Deep Learning

--- src/log_production_deep.py
+++ src/log_production_deep.py
@@ -16,7 +16,6 @@ from mlflow.tracking import MlflowClient
 import joblib
 from pprint import pprint
 
-
 def log_production_deep(config_file):
     config = get_data_deep(config_file)
     mlflow_config_deep = config["mlflow_config_deep"]
@@ -24,33 +23,36 @@ def log_production_deep(config_file):
     remote_server_uri_deep = mlflow_config_deep["remote_server_uri_deep"]
     mlflow.set_tracking_uri(remote_server_uri_deep)
     mlflow.set_experiment(mlflow_config_deep["experiment_name_deep"])
-    #runs = mlflow.search_runs([1])
-    #number = [1]
-    runs=mlflow.search_runs(experiment_ids=[2])
-    lowest=runs["metrics.train_accuracy"].sort_values(ascending=True)[0]
-    lowest_run_id=runs[runs["metrics.train_accuracy"]==lowest]["run_id"][0]
-
+    
+    runs = mlflow.search_runs(experiment_ids='2')
+    lowest = runs["params.train_accuracy"].sort_values(ascending=True)[0]
+    lowest_run_id = runs[runs["params.train_accuracy"] == lowest]["run_id"][0]
     client = MlflowClient()
+    logged_model = None
+    
 
     for mv in client.search_model_versions(f"name='{model_name}'"):
+        
         mv = dict(mv)
         if mv["run_id"] == lowest_run_id:
-            current_version=mv["version"]
+            current_version = mv["version"]
             logged_model = mv["source"]
             pprint(mv, indent=4)
             client.transition_model_version_stage(name=model_name, version=current_version, stage="Production")
         else:
-            current_version=mv["version"]
+            current_version = mv["version"]
             client.transition_model_version_stage(name=model_name, version=current_version, stage="Staging")
     
-    loaded_model = mlflow.pyfunc.load_model(logged_model)
-    model_path=mlflow_config_deep["model"]["save_dir"]
-    joblib.dump(loaded_model, model_path)
-
+    if logged_model is not None:
+        loaded_model = mlflow.pyfunc.load_model(logged_model)
+        model = config["model"]
+        model_path = model["save_dir"]
+        joblib.dump(loaded_model, model_path)
+    else:
+        print("No logged model found.")
 
 if __name__ == '__main__':
-    
     args_parser = argparse.ArgumentParser()
-    args_parser.add_argument('--config',default='deep_params.yaml')
+    args_parser.add_argument('--config', default='deep_params.yaml')
     passed_args = args_parser.parse_args()
     log_production_deep(config_file=passed_args.config)
--- webapp/firstApp/url.py
+++ webapp/firstApp/url.py
@@ -4,9 +4,10 @@ from firstApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('index/', views.result, name='result'),
     path('', views.SignupPage, name='signup'),
     path('login/', views.LoginPage, name='login'),
-    path('logout/', views.LogoutPage, name='logout'),    
+    path('logout/', views.LogoutPage, name='logout'),
+    path('index_deep/', views.index_deep, name='deep'),    
 ]
